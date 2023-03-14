79a16948495cb79e9e78993810db5c6d88ac4c86
Neokyuubi <40678329+neokyuubi@users.noreply.github.com>
2023-03-14 04:03:36 +0100

    custom css nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import React from "react";
 function Navbar()
 {
     return (
-        <h1>Navbar component</h1>
+        <nav>
+            <img src="src/assets/react-logo.png" className="nav--icon" alt="logo" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
     );
 
 }
 
-export default Navbar; 
+export default Navbar;
